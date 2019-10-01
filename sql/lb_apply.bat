e4feb0b9461460d406a734ebc9875277beae0626
Vadim Vlasov <stea1th@mail.ru>
2019-10-01 15:42:33 +0200

    6 1 HW5 model sql bat

--- sql/lb_apply.bat
+++ sql/lb_apply.bat
@@ -1,4 +1,4 @@
-set LB_HOME=c:\java\liquibase-3.5.3
+set LB_HOME=C:\Program Files\liquibase-3.8.0-bin
 call %LB_HOME%\liquibase.bat --driver=org.postgresql.Driver ^
 --classpath=%LB_HOME%\lib ^
 --changeLogFile=databaseChangeLog.sql ^
