54488f5a2c374eb655c956a8b203e59621f42a4f
Vadim Vlasov <stea1th@mail.ru>
2019-10-11 13:16:38 +0200

    Configs add

--- common/src/main/java/ru/javaops/masterjava/config/Configs.java
+++ common/src/main/java/ru/javaops/masterjava/config/Configs.java
@@ -3,6 +3,8 @@ package ru.javaops.masterjava.config;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
+import java.io.File;
+
 /**
  * gkislin
  * 01.11.2016
@@ -15,5 +17,10 @@ public class Configs {
 
     public static Config getConfig(String resource, String domain) {
         return getConfig(resource).getConfig(domain);
+    }
+
+    public static File getConfigFile(String fileName) {
+        Config conf = ConfigFactory.parseResources("../resources/wsdls.conf");
+        return new File(conf.getString("root") + fileName);
     }
 }
--- services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServicePublisher.java
+++ services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServicePublisher.java
@@ -1,6 +1,7 @@
 package ru.javaops.masterjava.service.mail;
 
 import com.google.common.collect.ImmutableList;
+import ru.javaops.masterjava.config.Configs;
 import ru.javaops.masterjava.persist.DBITestProvider;
 
 import javax.xml.transform.Source;
@@ -17,7 +18,7 @@ public class MailServicePublisher {
         Endpoint endpoint = Endpoint.create(new MailServiceImpl());
         List<Source> metadata = ImmutableList.of(
                 new StreamSource(
-                        new File("services/mail-service/src/main/webapp/WEB-INF/wsdl/mailService.wsdl")));
+                        Configs.getConfigFile("/wsdl/mailService.wsdl")));
 
         endpoint.setMetadata(metadata);
         endpoint.publish("http://localhost:8080/mail/mailService");
